733a0aa0e80949ca915d92a8a40822829e7fa636
Michael Samson <40731418+abush46@users.noreply.github.com>
2025-11-01 21:58:44 +0300

    Implement user registration and login endpoints
    Added authentication routes for user registration and login.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 
 const app = express()
+const { register, login, updateUser, deleteUser, userById, resetPassword } = require("./controllers/auth/auth");
 
 app.get('/', (_req, res) => {
   res.send('Hello Express!')
 })
+
+app.post('/register', register);
+app.post("/login", login)
+
 
 app.get('/api/users/:id', (_req, res) => {
   res.json({ id: _req.params.id })
