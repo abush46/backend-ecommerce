353ddc39fab2898d5ca9cd8249b109caa9fba1ce
Michael Samson <40731418+abush46@users.noreply.github.com>
2025-11-02 01:02:20 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,19 +11,18 @@ app.get('/', (_req, res) => {
   res.send('Hello Express!')
 })
 
-app.post('/api/data', createUser);
-//=> {
+app.post('/api/data', createUser)=> {
     // Access the request body
-  //  const requestData = req.body; 
+   const requestData = req.body; 
 
-   // console.log('Received data:', requestData);
+   console.log('Received data:', requestData);
 
     // Process the data (e.g., save to a database)
     // ...
 
     // Send a response back to the client
-  //  res.status(201).send({ message: 'Data received successfully!', data: requestData });
-//});
+  res.status(201).send({ message: 'Data received successfully!', data: requestData });
+});
 
 
 app.get('/api/users/:id', (_req, res) => {
