1b17eee5c32dc8aa0f8582b87e2d1195bdb658e9
Michael Samson <40731418+abush46@users.noreply.github.com>
2025-11-01 23:58:49 +0300

    Implement user login with JWT authentication

---
+++ controllers/auth.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = "seceret";
+
+function generateAuthToken(data){
+  const token = jwt.sign(data, JWT_SECRET_KEY, { expiresIn: '10h' })
+  return token
+}
+module.exports.login = async (req, res) => {
+
+   // Access the request body
+    const requestData = req.body; 
+
+   // console.log('Received data:', requestData);
+
+    // Process the data (e.g., save to a database)
+    // ...
+
+    // Send a response back to the client
+  // res.status(201).send({ message: 'Data received successfully!', data: requestData });
+  return res.json({
+        success: true,
+        status: 200,
+        message: "user Logged in",
+        data: requestData,
+      });
+}
