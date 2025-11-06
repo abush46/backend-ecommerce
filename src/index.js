78c9cc9d194d7cee8bb496d0511eeb1998a62edc
abush46 <40731418+abush46@users.noreply.github.com>
2025-11-07 00:00:41 +0300

    api

--- src/index.js
+++ src/index.js
@@ -66,51 +66,51 @@ app.post('/api/data', (req, res) => {
 })
 
 // AUTH
-app.post('/register', register);
-app.post("/login", login)
+app.post('/api/register', register);
+app.post("/api/login", login)
 
 
 // User Routes
-app.post("/update-user", updateUser)
-app.get("/user", userById)
-app.get("/delete-user", deleteUser)
-app.post("/reset-password", resetPassword)
+app.post("/api/update-user", updateUser)
+app.get("/api/user", userById)
+app.get("/api/delete-user", deleteUser)
+app.post("/api/reset-password", resetPassword)
 
 
 // Products
-app.post("/product", [isAdmin], addProduct)
-app.post("/user-add-product",[checkAuth], UseraddProduct)
-app.get("/products", getAllProducts)
-app.post("/update-product", [isAdmin], updateProduct)
-app.get("/delete-product", [isAdmin], deleteProduct)
-
+app.post("/api/product", [isAdmin], addProduct)
+app.post("/api/user-add-product",[checkAuth], UseraddProduct)
+app.get("/api/products", getAllProducts)
+app.post("/api/update-product", [isAdmin], updateProduct)
+app.get("/api/delete-product", [isAdmin], deleteProduct)
+/api
 
 // CATEGORIES
-app.post("/category", [isAdmin], addCategory)
-app.get("/categories", getCategories)
-app.post("/update-category", [isAdmin], updateCategory)
-app.get("/delete-category", [isAdmin], deleteCategory)
+app.post("/api/category", [isAdmin], addCategory)
+app.get("/api/categories", getCategories)
+app.post("/api/update-category", [isAdmin], updateCategory)
+app.get("/api/delete-category", [isAdmin], deleteCategory)
 
 
 // ORDERS
-app.get("/orders",[checkAuth],orders)
+app.get("/api/orders",[checkAuth],orders)
 
 // CHECKOUT
-app.post("/checkout",[checkAuth],checkout)
+app.post("/api/checkout",[checkAuth],checkout)
 
 // WISHLIST
-app.post("/add-to-wishlist",[checkAuth],addToWishlist)
-app.get("/wishlist",[checkAuth],wishlist)
-app.get("/remove-from-wishlist",[checkAuth],removeFromWishlist)
+app.post("/api/add-to-wishlist",[checkAuth],addToWishlist)
+app.get("/api/wishlist",[checkAuth],wishlist)
+app.get("/api/remove-from-wishlist",[checkAuth],removeFromWishlist)
 
 // ADMIN
-app.get("/dashboard",[isAdmin],dashboardData)
-app.get("/admin/orders",[isAdmin],getAllOrders)
-app.get("/admin/order-status",[isAdmin],changeStatusOfOrder)
-app.get("/admin/users",[isAdmin],getAllUsers)
+app.get("/api/dashboard",[isAdmin],dashboardData)
+app.get("/api/admin/orders",[isAdmin],getAllOrders)
+app.get("/api/admin/order-status",[isAdmin],changeStatusOfOrder)
+app.get("/api/admin/users",[isAdmin],getAllUsers)
 
 // HELPER
-app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {  
+app.post('/api/photos/upload', upload.array('photos', 12), function (req, res, next) {  
   // req.files is array of `photos` files
 
   try{
