9600646ea9756bf1418cfb4c36915d91136352d5
abush46 <40731418+abush46@users.noreply.github.com>
2025-11-06 23:50:19 +0300

    test

--- src/controllers/test/testController.js
+++ src/controllers/test/testController.js
@@ -6,7 +6,7 @@ module.exports.getProducts = async (req, res) => {
         /* const products = await productModel.find();
         const productsCount = await productModel.find().count(); */
 
-        return res.json({
+         res.json({
             success : true,
             status : 400,
             message : "list of all products",
@@ -16,6 +16,6 @@ module.exports.getProducts = async (req, res) => {
         })
 
     }catch(error){
-        return res.json("Error h:",error.message)
+         res.json("Error h:",error.message)
     }
 }
