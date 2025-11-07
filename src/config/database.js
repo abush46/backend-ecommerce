ccc02990a41ea85f4b1867d2d54dcf968a333304
abush46 <40731418+abush46@users.noreply.github.com>
2025-11-08 00:33:40 +0300

    db

--- src/config/database.js
+++ src/config/database.js
@@ -15,24 +15,16 @@ module.exports.connection = async () => {
   //mongodb+srv://mic:abush1157@cluster0.9bamq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
   //"mongodb+srv://mic:abush1157@cluster0.9bamq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  try {
-  await mogoose.connect(conn);
-    // Send a ping to confirm a successful connection
-   // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-    console.log("DB Connection Successfull");
-  }
-
-  /* mogoose
+ 
+   
+ 
+   mogoose
     .connect(
       conn
-    ) */
+    ) 
     //mongodb + srv://abidrazaa:Abcd1234@cluster0.lr2rk.mongodb.net/?retryWrites=true&w=majority")
-   // .then(() => console.log("DB Connection Successfull"))
-    //.catch((err) => console.log(err.message));
+    .then(() => console.log("DB Connection Successfull"))
+    .catch((err) => console.log(err.message));
 };
 
 
