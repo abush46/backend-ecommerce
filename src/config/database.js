92245dbee6bcf26272c7462fcd5742aa20a0fe03
abush46 <40731418+abush46@users.noreply.github.com>
2025-11-08 00:22:41 +0300

    db

--- src/config/database.js
+++ src/config/database.js
@@ -3,26 +3,27 @@ const {mogoose,MongoClient, ServerApiVersion} = require("mongoose");
 
 const { DB_CON_STRING } = process.env;
 const conn = "mongodb+srv://mic:abush1157@cluster0.9bamq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-  const client = new MongoClient(conn, {
+ /*  const client = new MongoClient(conn, {
   serverApi: {
    // version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
   }
-  });
+  }); */
 module.exports.connection = async () => {
   // mogoose.connect("mongodb://localhost/ecommerce")
   //mongodb+srv://mic:abush1157@cluster0.9bamq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
 
   //"mongodb+srv://mic:abush1157@cluster0.9bamq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   try {
-  await client.connect();
+  await mogoose.connect(conn);
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+   // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
+    console.log("DB Connection Successfull");
   }
 
   /* mogoose
